63d0040f4882ce218d301f29adabb80c87f49db4
EC2 Default User <ec2-user@ip-172-31-46-112.us-west-2.compute.internal>
2023-05-19 23:40:09 +0000

    update productionrb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,9 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
-  config.hosts = [
-    "salishdistro.com"
-  ]
+  config.hosts.clear
   # Code is not reloaded between requests.
   config.cache_classes = true
 
