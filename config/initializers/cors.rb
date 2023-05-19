56789d18c4750d0034f9723c69969ae54dcd198f
EC2 Default User <ec2-user@ip-172-31-46-112.us-west-2.compute.internal>
2023-05-19 20:04:12 +0000

    comment cors back in

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,13 +15,13 @@
 #   end
 # end
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#     allow do
-#       origins 'http://localhost:3000', 'http://localhost:3001', 'https://salishdistro.com/'
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3000', 'http://localhost:3001', 'https://salishdistro.com/'
   
-#       resource '*',
-#         headers: :any,
-#         methods: [:get, :post, :put, :patch, :delete, :options, :head],
-#         credentials: true
-#     end
-#   end
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+  end
